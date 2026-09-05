401ebc99b329eca95fc98fe2851e125d0efb4618
bc69s2zg4s-alt <bc69s2zg4s@privaterelay.appleid.com>
2026-09-06 03:43:37 +0500

    Update hud.js

--- js/hud.js
+++ js/hud.js
@@ -1,40 +1,32 @@
 "use strict";
-
 const HUD = {
     level: 1,
     gold: 0,
     crystals: 0,
-
     update() {
         document.getElementById("levelValue").textContent = this.level;
         document.getElementById("goldValue").textContent = this.gold;
         document.getElementById("crystalValue").textContent = this.crystals;
     },
-
     setLevel(value) {
         this.level = value;
         this.update();
     },
-
     setGold(value) {
         this.gold = value;
         this.update();
     },
-
     setCrystals(value) {
         this.crystals = value;
         this.update();
     },
-
     addGold(value) {
         this.gold += value;
         this.update();
     },
-
     addCrystals(value) {
         this.crystals += value;
         this.update();
     }
 };
-
 HUD.update();
