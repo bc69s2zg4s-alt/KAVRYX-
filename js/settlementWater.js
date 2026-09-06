850abbf35b2f4a3ebc69f11a0a852d12a99539df
bc69s2zg4s-alt <bc69s2zg4s@privaterelay.appleid.com>
2026-09-06 05:06:13 +0500

    Update settlementWater.js

--- js/settlementWater.js
+++ js/settlementWater.js
@@ -6,45 +6,53 @@ const SettlementWater={
         const top=centerY-55;
         const right=left+size;
         const bottom=top+size*.7;
-        this.drawPond(ctx,left+105,top+135,118,72);
-        this.drawPond(ctx,right-145,bottom-100,92,58);
+        this.drawPond(ctx,left+110,top+145,150,82);
+        this.drawPond(ctx,right-125,bottom-70,115,62);
     },
     drawPond(ctx,x,y,width,height){
-        ctx.fillStyle="#151918";
-        ctx.fillRect(x-width/2-8,y-height/2-8,width+16,height+16);
-        ctx.fillStyle="#303b39";
-        ctx.fillRect(x-width/2-4,y-height/2-4,width+8,height+8);
-        ctx.fillStyle="#253b3c";
+        ctx.fillStyle="#121716";
+        ctx.fillRect(x-width/2-9,y-height/2-9,width+18,height+18);
+        ctx.fillStyle="#514b3c";
+        ctx.fillRect(x-width/2-5,y-height/2-5,width+10,height+10);
+        ctx.fillStyle="#263b3c";
         ctx.fillRect(x-width/2,y-height/2,width,height);
-        ctx.fillStyle="#304d4e";
-        ctx.fillRect(x-width*.35,y-height*.25,width*.7,7);
-        ctx.fillRect(x-width*.18,y-height*.4,width*.36,5);
-        ctx.fillStyle="#3d5c59";
-        ctx.fillRect(x-width*.25,y+height*.18,width*.45,4);
-        this.drawShore(ctx,x,y,width,height);
-        this.drawWaterRocks(ctx,x,y,width,height);
+        ctx.fillStyle="#304b4b";
+        ctx.fillRect(x-width*.36,y-height*.25,width*.65,6);
+        ctx.fillRect(x-width*.18,y-height*.4,width*.4,4);
+        ctx.fillStyle="#3d5b58";
+        ctx.fillRect(x-width*.3,y+height*.2,width*.48,4);
+        ctx.fillStyle="#182a2b";
+        ctx.fillRect(x-width*.42,y-height*.05,width*.22,5);
+        ctx.fillRect(x+width*.2,y+height*.1,width*.18,4);
+        this.drawRocks(ctx,x,y,width,height);
+        this.drawReeds(ctx,x,y,width,height);
     },
-    drawShore(ctx,x,y,width,height){
-        ctx.fillStyle="#514b3b";
-        ctx.fillRect(x-width/2-7,y-height/2-3,7,6);
-        ctx.fillRect(x+width/2,y-height/2-3,7,6);
-        ctx.fillRect(x-width/2-7,y+height/2-3,7,6);
-        ctx.fillRect(x+width/2,y+height/2-3,7,6);
-        ctx.fillRect(x-width*.25,y-height/2-6,42,6);
-        ctx.fillRect(x+width*.08,y+height/2,45,6);
-    },
-    drawWaterRocks(ctx,x,y,width,height){
+    drawRocks(ctx,x,y,width,height){
         const rocks=[
             [x-width*.35,y-height*.38,7],
-            [x+width*.36,y-height*.12,5],
-            [x-width*.28,y+height*.36,6],
-            [x+width*.3,y+height*.3,7]
+            [x+width*.37,y-height*.12,6],
+            [x-width*.3,y+height*.36,6],
+            [x+width*.3,y+height*.32,8]
         ];
         rocks.forEach((rock)=>{
-            ctx.fillStyle="#252b29";
+            ctx.fillStyle="#202522";
             ctx.fillRect(rock[0]-rock[2],rock[1]-rock[2]/2,rock[2]*2,rock[2]);
             ctx.fillStyle="#4a514c";
-            ctx.fillRect(rock[0]-rock[2]*.5,rock[1]-rock[2]*.65,rock[2],rock[2]*.35);
+            ctx.fillRect(rock[0]-rock[2]*.5,rock[1]-rock[2]*.65,rock[2],rock[2]*.3);
+        });
+    },
+    drawReeds(ctx,x,y,width,height){
+        const points=[
+            [x-width*.46,y-height*.35],
+            [x+width*.46,y-height*.2],
+            [x-width*.4,y+height*.25],
+            [x+width*.4,y+height*.3]
+        ];
+        points.forEach((point)=>{
+            ctx.fillStyle="#354335";
+            ctx.fillRect(point[0],point[1]-12,3,12);
+            ctx.fillRect(point[0]+5,point[1]-9,3,9);
+            ctx.fillRect(point[0]+9,point[1]-14,3,14);
         });
     }
 };
