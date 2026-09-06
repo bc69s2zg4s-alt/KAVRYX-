ed9b69b9359d65cb54d0090344a84a6404ea8d8e
bc69s2zg4s-alt <bc69s2zg4s@privaterelay.appleid.com>
2026-09-06 05:24:07 +0500

    Update settlementGround.js

--- js/settlementGround.js
+++ js/settlementGround.js
@@ -21,17 +21,25 @@ const SettlementGround={
             const y=map.top+random()*map.height;
             const size=4+random()*14;
             const type=Math.floor(random()*5);
-            ctx.fillStyle=type===0?"#202522":type===1?"#242a25":type===2?"#2d352d":type===3?"#292b27":"#202522";
-            if(type===0)ctx.fillRect(x,y,size,size*.55);
+            if(map.inRoads(x,y,2))continue;
+            if(type===0){
+                ctx.fillStyle="#202522";
+                ctx.fillRect(x,y,size,size*.55);
+                ctx.fillStyle="#303631";
+                ctx.fillRect(x+2,y,size*.55,2);
+            }
             if(type===1){
+                ctx.fillStyle="#242a25";
                 ctx.fillRect(x,y,size*.7,size*.35);
                 ctx.fillRect(x+size*.5,y+size*.2,size*.5,2);
             }
             if(type===2){
+                ctx.fillStyle="#2d352d";
                 ctx.fillRect(x,y,size*.35,size);
                 ctx.fillRect(x+size*.25,y-size*.35,2,size*.7);
             }
             if(type===3){
+                ctx.fillStyle="#292b27";
                 ctx.fillRect(x,y,size,size*.7);
                 ctx.fillStyle="#36382f";
                 ctx.fillRect(x+2,y+2,size*.45,2);
@@ -53,31 +61,39 @@ const SettlementGround={
             ctx.fillRect(road.x-road.w/2,road.y-road.h/2,road.w,road.h);
         });
         ctx.fillStyle="#3a3d35";
-        for(let x=map.left;x<map.right;x+=38){
-            if(Math.abs(x)<18)continue;
-            ctx.fillRect(x,map.roads[0].y-6,24,10);
-        }
-        for(let y=map.top;y<65;y+=38){
-            if(Math.abs(y-65)<20)continue;
-            ctx.fillRect(-5,y,10,25);
-        }
+        map.roads.forEach((road)=>{
+            if(road.w>road.h){
+                for(let x=road.x-road.w/2+10;x<road.x+road.w/2;x+=38){
+                    ctx.fillRect(x,road.y-5,24,9);
+                }
+            }else{
+                for(let y=road.y-road.h/2+10;y<road.y+road.h/2;y+=38){
+                    ctx.fillRect(road.x-5,y,10,25);
+                }
+            }
+        });
         ctx.fillStyle="#171a18";
-        ctx.fillRect(map.left,map.roads[0].y-21,map.width,4);
-        ctx.fillRect(map.left,map.roads[0].y+17,map.width,4);
-        ctx.fillRect(-19,map.top,4,335);
-        ctx.fillRect(15,map.top,4,335);
+        map.roads.forEach((road)=>{
+            if(road.w>road.h){
+                ctx.fillRect(road.x-road.w/2,road.y-road.h/2,road.w,4);
+                ctx.fillRect(road.x-road.w/2,road.y+road.h/2-4,road.w,4);
+            }else{
+                ctx.fillRect(road.x-road.w/2,road.y-road.h/2,4,road.h);
+                ctx.fillRect(road.x+road.w/2-4,road.y-road.h/2,4,road.h);
+            }
+        });
     },
     drawEdges(ctx,map){
         ctx.fillStyle="#252b27";
-        ctx.fillRect(map.left,map.bottom-28,map.width,28);
         ctx.fillRect(map.left,map.top,map.width,22);
+        ctx.fillRect(map.left,map.bottom-28,map.width,28);
         ctx.fillStyle="#101312";
-        ctx.fillRect(map.left,map.bottom-5,map.width,5);
         ctx.fillRect(map.left,map.top,map.width,5);
+        ctx.fillRect(map.left,map.bottom-5,map.width,5);
         for(let x=map.left+10;x<map.right;x+=52){
             ctx.fillStyle="#343a35";
-            ctx.fillRect(x,map.bottom-20,28,6);
             ctx.fillRect(x,map.top+9,28,6);
+            ctx.fillRect(x,map.bottom-20,28,6);
         }
     }
 };
